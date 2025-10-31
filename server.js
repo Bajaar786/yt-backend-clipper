9c21c256924109c270cb29d5101d18a0ecadedc2
DELL@DESKTOP-NO1CARES <m.mehboobelahi1@gmail.com>
2025-10-31 20:30:28 +0500

    Fix CORS for Chrome extension

--- server.js
+++ server.js
@@ -23,17 +23,21 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: function(origin, callback){
-    // allow requests with no origin (like mobile apps or curl)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
+    // allow requests with no origin (like Chrome extension preflights)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
       const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
       return callback(new Error(msg), false);
     }
-    return callback(null, true);
   },
   methods: ["GET", "POST", "OPTIONS"],
-  allowedHeaders: ["Content-Type"]
+  allowedHeaders: ["Content-Type"],
+  credentials: true
 }));
+
 
 // handle preflight requests (important for POST)
 app.options("*", cors());
