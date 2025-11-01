00865a9c5370233752304e30ff36b06aeaf9f58d
DELL@DESKTOP-NO1CARES <m.mehboobelahi1@gmail.com>
2025-11-01 10:45:40 +0500

    push server

--- server.js
+++ server.js
@@ -58,5 +58,5 @@ app.post("/summarize", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
